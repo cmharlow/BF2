c35d9641210bd3f8305b40c5b73d4325a4b8cfc2
cmh2166 <cmharlow@gmail.com>
2016-04-28 18:47:18 -0400

    changed from test time to hourly

--- grabBF2.py
+++ grabBF2.py
@@ -6,7 +6,7 @@ import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=60)
 def scheduled_job():
     main()
 
